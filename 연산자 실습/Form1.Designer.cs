a60debd6f98396888e6628e1e39bcdaf04c4a58b
Seochangbum <ckdqja1115@naver.com>
2025-04-28 12:27:43 +0900

    commit3

--- 연산자 실습/Form1.Designer.cs
+++ 연산자 실습/Form1.Designer.cs
@@ -29,22 +29,44 @@
         private void InitializeComponent()
         {
             this.textBox_Print = new System.Windows.Forms.TextBox();
+            this.textBox_print2 = new System.Windows.Forms.TextBox();
+            this.textBox_print3 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // textBox_Print
             // 
-            this.textBox_Print.Location = new System.Drawing.Point(43, 26);
+            this.textBox_Print.Location = new System.Drawing.Point(12, 26);
             this.textBox_Print.Multiline = true;
             this.textBox_Print.Name = "textBox_Print";
-            this.textBox_Print.Size = new System.Drawing.Size(711, 386);
+            this.textBox_Print.Size = new System.Drawing.Size(253, 386);
             this.textBox_Print.TabIndex = 0;
             this.textBox_Print.TextChanged += new System.EventHandler(this.textBox_Print_TextChanged);
+            // 
+            // textBox_print2
+            // 
+            this.textBox_print2.Location = new System.Drawing.Point(271, 26);
+            this.textBox_print2.Multiline = true;
+            this.textBox_print2.Name = "textBox_print2";
+            this.textBox_print2.Size = new System.Drawing.Size(256, 386);
+            this.textBox_print2.TabIndex = 1;
+            this.textBox_print2.TextChanged += new System.EventHandler(this.textBox_print2_TextChanged);
+            // 
+            // textBox_print3
+            // 
+            this.textBox_print3.Location = new System.Drawing.Point(533, 26);
+            this.textBox_print3.Multiline = true;
+            this.textBox_print3.Name = "textBox_print3";
+            this.textBox_print3.Size = new System.Drawing.Size(255, 386);
+            this.textBox_print3.TabIndex = 2;
+            this.textBox_print3.TextChanged += new System.EventHandler(this.textBox_print3_TextChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox_print3);
+            this.Controls.Add(this.textBox_print2);
             this.Controls.Add(this.textBox_Print);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,6 +78,8 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBox_Print;
+        private System.Windows.Forms.TextBox textBox_print2;
+        private System.Windows.Forms.TextBox textBox_print3;
     }
 }
 
--- 연산자 실습/Form1.cs
+++ 연산자 실습/Form1.cs
@@ -15,7 +15,10 @@ namespace 연산자_실습
         public Form1()
         {
             InitializeComponent();
-            /*string fruits = "appple";
+
+
+            /*Q1
+             * string fruits = "appple";
             int count = 5;
             int price = 2000;
 
@@ -25,18 +28,39 @@ namespace 연산자_실습
             */
 
 
-            string num = "15";
+            /*Q2
+             * string num = "15";
             int num2; int num3;
             num2 = int.Parse(num);
             num2 += 10;
             //num3 = num2 + 10;
 
             textBox_Print.Text += "15에 10을 더하면" + num2 + "입니다.";
+            */
+
+
+            string productName = "노트북";
+            int price = 1200000;
+            float discountPercent = 0.15f;
+            byte stocks = 8;
+
+            string isAvailable = (stocks > 1) ? "True" : "False";
+
+            double discountPrice = price * (1 - discountPercent);
+
+            textBox_Print.Text += (stocks > 0) ? "구매 가능: 할인 가격은" + discountPrice.ToString() + "입니다" : "품절입니다.";
+
+            textBox_print2.Text = (stocks > 5) ? "여유 있음" : "소량 남음";
+
+            textBox_print3.Text = "상품명:" + productName + ", 할인된 가격:" + discountPrice.ToString() + "원, 재고:" + stocks.ToString() + "개";
+
+
+
+
         }
 
         private void textBox_Print_TextChanged(object sender, EventArgs e)
-        {
-
+        }
         }
     }
 }
