0ff640f81f505842aa5528575aab8390d2745d25
Seochangbum <ckdqja1115@naver.com>
2025-04-28 11:34:42 +0900

    commit2

--- 연산자 실습/Form1.cs
+++ 연산자 실습/Form1.cs
@@ -15,6 +15,28 @@ namespace 연산자_실습
         public Form1()
         {
             InitializeComponent();
+            /*string fruits = "appple";
+            int count = 5;
+            int price = 2000;
+
+            int totalPrice = count * price;
+
+            textBox_Print.Text += "사과 총 5개의 가격은 :" + totalPrice.ToString() + "원입니다.";
+            */
+
+
+            string num = "15";
+            int num2; int num3;
+            num2 = int.Parse(num);
+            num2 += 10;
+            //num3 = num2 + 10;
+
+            textBox_Print.Text += "15에 10을 더하면" + num2 + "입니다.";
+        }
+
+        private void textBox_Print_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
