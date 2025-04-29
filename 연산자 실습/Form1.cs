fdc53fcacedf32bc07557460014173d400a2b6c5
Seochangbum <ckdqja1115@naver.com>
2025-04-29 09:19:09 +0900

    commit 1234

--- 연산자 실습/Form1.cs
+++ 연산자 실습/Form1.cs
@@ -38,8 +38,8 @@ namespace 연산자_실습
             textBox_Print.Text += "15에 10을 더하면" + num2 + "입니다.";
             */
 
-
-            string productName = "노트북";
+            //Q3,4,5
+            /*string productName = "노트북";
             int price = 1200000;
             float discountPercent = 0.15f;
             byte stocks = 8;
@@ -52,15 +52,66 @@ namespace 연산자_실습
 
             textBox_print2.Text = (stocks > 5) ? "여유 있음" : "소량 남음";
 
-            textBox_print3.Text = "상품명:" + productName + ", 할인된 가격:" + discountPrice.ToString() + "원, 재고:" + stocks.ToString() + "개";
+            textBox_print3.Text = "상품명:" productName + ", 할인된 가격:" + discountPrice .ToString() + "원, 재고:" + stocks.ToString() + ""입니다
+            */
+
+
+            //중간 실습
+            /*int[,,] num2 = new int[,,]
+            {
+                {
+                    { 1, 2, 3 }, { 4, 5, 6 },
+                },
+                {
+                    { 7, 8, 9 }, { 10, 11, 12 }
+
+                }
+            };
+            textBox_Print.Text = num2[1, 0, 1].ToString();
+            */
+
+            string[][] jaggedArray = new string[3][];
+            jaggedArray[0] = new string[2] { "카리나", "윈터" };
+            jaggedArray[1] = new string[3] { "닝닝", "지젤", "장원영" };
+            jaggedArray[2] = new string[1] { "안유진"};
+
+
+            textBox_Print.Text += "1반 학생 목록: \r\n";
+            textBox_Print.Text += "-" + jaggedArray[0][0]; 
+            textBox_Print.Text += "\r\n";
+            textBox_Print.Text += "-" + jaggedArray[0][1];
+            textBox_Print.Text += "\r\n";
+            textBox_Print.Text += "2반 학생 목록: \r\n";
+            textBox_Print.Text += "-" + jaggedArray[1][0]; 
+            textBox_Print.Text += "\r\n";
+            textBox_Print.Text += "-" + jaggedArray[1][1]; 
+            textBox_Print.Text += "\r\n";
+            textBox_Print.Text += "-" + jaggedArray[1][2];
+            textBox_Print.Text += "\r\n";
+            textBox_Print.Text += "3반 학생 목록: \r\n";
+            textBox_Print.Text += "-" + jaggedArray[2][0];
+             
+
+
+            ;
 
 
 
 
         }
-
         private void textBox_Print_TextChanged(object sender, EventArgs e)
+        {
+
         }
+
+        private void textBox_print3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox_print2_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
